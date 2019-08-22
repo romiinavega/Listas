8b06e70ea64c5735444de75ded09111f2c65da6a
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 08:41:13 -0700

    Agregar archivos de proyecto.

---
+++ Lisstas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisstas
+{
+    class Program
+    {
+        private static object indiceColor;
+
+        static void Main(string[] args)
+        {
+            List<string> colores =
+                new List<string>();
+            colores.Add("rojo");
+            colores.Add("verde");
+            colores.Add("rosa");
+            colores.Add("azul");
+
+            colores.RemoveAt(1);
+
+            Console.WriteLine("Numero de elementos: " + colores.Count);
+            int indiceColor = 1;
+        foreach(string color in colores)
+
+            {
+                Console.WriteLine ("Elemento " + indiceColor.ToString() + ";" + color );
+                indiceColor++;
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
